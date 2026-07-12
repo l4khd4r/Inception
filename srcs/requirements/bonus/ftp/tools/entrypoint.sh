631bc34ea7202e444839289407c3c85394fef1bc
mlakhdar <mohamedlakhdar.sec@gmail.com>
2026-07-12 13:36:51 -0500

    feat(ftp): done the ftp, the user mlakhdar can login into the server from the hosts and get the files of the wordpress, thems , config files. ....

--- srcs/requirements/bonus/ftp/tools/entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/entrypoint.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
-
+set -e
 
 mkdir -p /var/run/vsftpd/empty
+mkdir -p /var/www/html
 
-# Create a system user for FTP if it doesn't exist
-if [ ! -d "/home/$FTP_USER" ]; then
-    
-    useradd -d /var/www/html -s /usr/sbin/bash "$FTP_USER"
+if ! id "$FTP_USER" >/dev/null 2>&1; then
+    useradd -m -d /var/www/html -s /bin/bash "$FTP_USER"
     echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
-    
-    chown -R "$FTP_USER:$FTP_USER" /var/www/html
 fi
 
-echo "FTP Server starting..."
+chown -R "$FTP_USER:$FTP_USER" /var/www/html
+
+echo "Starting vsftpd..."
 exec /usr/sbin/vsftpd /etc/vsftpd.conf
